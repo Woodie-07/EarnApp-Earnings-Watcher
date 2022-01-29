241039b841c83841d0b9af55ec387b23995a4cdf
Woodie-07 <40068306+Woodie-07@users.noreply.github.com>
2022-01-29 19:53:12 +0000

    Added GNU GPL v3
    This software is now licensed under the GNU General Public Licence v3

--- log.py
+++ log.py
@@ -1,3 +1,16 @@
+"""
+EarnApp-Earnings-Watcher - A program to monitor your EarnApp Earnings and send data to a discord webhook
+Copyright (C) 2022  SWM Tech Industries
+
+This file is part of EarnApp-Earnings-Watcher.
+
+EarnApp-Earnings-Watcher is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+EarnApp-Earnings-Watcher is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with EarnApp-Earnings-Watcher. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import sys
 from colorama import Fore, Back, Style
 
